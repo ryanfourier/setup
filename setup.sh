378769f57daf526047f6ffea600a5220e55016e6
ryanfourier <ryan.hammang@gmail.com>
2014-10-10 14:43:09 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,19 @@ get chrome
 get insync
 make the other drives writable, and figure out how to use them.
 
+# 1. Add this line to your list of repositories by
+#    editing your /etc/apt/sources.list
+deb http://repository.spotify.com stable non-free
 
+# 2. If you want to verify the downloaded packages,
+#    you will need to add our public key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
+
+# 3. Run apt-get update
+sudo apt-get update
+
+# 4. Install spotify!
+sudo apt-get install spotify-client
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
